354e272466d356eb3aa19bcad9871c4370ad2712
ryryry <ry@yoooooo.com>
2016-09-30 10:27:17 +0800

    Initial Commit

---
+++ RYAccessAuthority/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RYAccessAuthority
+//
+//  Created by ruanyang on 16/9/30.
+//  Copyright © 2016年 mengtukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RYAccessAuthority/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RYAccessAuthority
+//
+//  Created by ruanyang on 16/9/30.
+//  Copyright © 2016年 mengtukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
